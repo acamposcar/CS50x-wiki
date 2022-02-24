4f5764f91b6ec2202e6f6d6d5a8cb300607aae8d
Alejandro Campos <acamposcar@gmail.com>
2022-02-24 02:43:35 +0100

    Start programming new entry and edit entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
 
 from . import util
 import random
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -21,10 +25,26 @@ def entry(request, title):
 # TODO        
 def new(request):
     if request.method == "POST":
-        return render(request, "encyclopedia/index.html")
-    else:
-        return render(request, "encyclopedia/new.html")
+        title = request.POST['title'].strip()
+        content = request.POST['content']
+        for existing_title in util.list_entries():
+            if title.upper() == existing_title.upper().strip():
+                return render(request, "encyclopedia/new.html", {
+                    "title": title,
+                    "content": content,
+                    "message": "This entry already exists. Choose another title."
+            })
 
+        content = f"# {title} \n\n {content}"
+        util.save_entry(title, content)
+        return HttpResponseRedirect(reverse("entry", kwargs={'title': title}))
+
+    else:
+        return render(request, "encyclopedia/new.html", {
+            "title": "",
+            "content": "",
+            "message": ""
+        })
 
 # TODO: redirect to entry url (use reverse)
 def random_entry(request):
@@ -34,7 +54,20 @@ def random_entry(request):
             "title": title,
             "entry": entry
         })
-        
+
 # TODO  
-def edit(request):
-    pass
+def edit(request, title):
+    if request.method == "POST":
+        content = request.POST['content']
+        util.save_entry(title, content)
+        return HttpResponseRedirect(reverse("entry", kwargs={'title': title}))
+
+    else:
+        entry = util.get_entry(title)
+        if entry:
+            return render(request, "encyclopedia/edit.html", {
+                "title": title,
+                "entry": entry
+            })
+        else:
+            return render(request, "encyclopedia/404.html")
